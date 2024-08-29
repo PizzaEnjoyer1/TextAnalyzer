a27a72b45a8e761b3564209ec282e5d00466f99d
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-08-29 10:32:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,13 @@ with st.expander('Analizar Polaridad y Subjetividad en un texto'):
 
         initialText = TextBlob(text1)
 
-        translatedText = TextBlob(translator.translate(initialText, src="es", dest="en"))
+        translation = translator.translate(initialText, src="es", dest="en")
+
+        blobText = TextBlob(translation.text)
         
-        st.write('Polarity: ', round(translatedText.sentiment.polarity,2))
-        st.write('Subjectivity: ', round(translatedText.sentiment.subjectivity,2))
-        x=round(translatedText.sentiment.polarity,2)
+        st.write('Polarity: ', round(blobText.sentiment.polarity,2))
+        st.write('Subjectivity: ', round(blobText.sentiment.subjectivity,2))
+        x=round(blobText.sentiment.polarity,2)
         if x >= 0.5:
             st.write( 'Es un sentimiento Positivo 😊')
         elif x <= -0.5:
